f909ab88f35683a69e1b31a48051917f9d82255e
Pashted <pashted9146@gmail.com>
2019-05-02 02:10:59 +0300

    Add slider js-module

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import "../less/template.less";
 import * as Header from "./header";
+import * as Slider from "./slider";
 
 window.addEventListener('resize', Header.setPositions);
 Header.setPositions();
+
+Slider.init();
