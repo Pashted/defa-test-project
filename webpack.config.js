af7319b5072714719c0d6c320a59844afd56e63d
Pashted <pashted9146@gmail.com>
2019-05-02 03:03:25 +0300

    Add minimise css option to production build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,46 @@
 const path = require('path'),
     Copy = require('copy-webpack-plugin'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin');
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    OptimizeCssnanoPlugin = require('@intervolga/optimize-cssnano-plugin');
 
-
-module.exports = {
-    mode:    'development',
-    devtool: 'source-map',
-    entry:   './src/js/index.js',
-    output:  {
-        path:     path.resolve('./public'),
-        filename: './scripts/bundle.js',
-    },
-    plugins: [
+let plugins = [
         new Copy([
             { from: 'src/html', to: './' },
             { from: 'src/icons/favicon.ico', to: './' },
         ]),
         new ExtractTextPlugin('template.css')
     ],
+
+    dev = true;
+
+process.argv.forEach(param => {
+    // если webpack запущен с командой --mode production
+    if (param === 'production')
+        dev = false;
+});
+
+if (!dev)
+    plugins.push(
+        new OptimizeCssnanoPlugin({
+            cssnanoOptions: {
+                preset: ['default', {
+                    discardComments: {
+                        removeAll: true,
+                    },
+                }],
+            },
+        })
+    );
+
+module.exports = {
+    mode:    dev ? 'development' : 'production',
+    devtool: dev ? 'source-map' : false,
+    entry:   './src/js/index.js',
+    output:  {
+        path:     path.resolve('./public'),
+        filename: './scripts/bundle.js',
+    },
+    plugins,
     module:  {
         rules: [
             {
